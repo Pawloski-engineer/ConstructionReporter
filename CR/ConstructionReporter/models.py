7338eeb4cd20016fb34ed4bbd7120e8271d96da5
pkbla <pkblasiak@gmail.com>
2021-04-14 20:12:14 +0200

    added creation_date field to defect

--- CR/ConstructionReporter/models.py
+++ CR/ConstructionReporter/models.py
@@ -68,11 +68,7 @@ class Defect(models.Model):
     defect_status = models.ForeignKey(DefectStatus, on_delete=models.CASCADE)    #TODO make it so that user decides which one to choose
     defect_location = models.ForeignKey(Location, on_delete=models.CASCADE)
     defect_respondent = models.ManyToManyField(Group, blank=True, null=True)
-    # creation_date = models.DateTimeField('date created')
-
-    # creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-
-    # creation_date = models.DateTimeField('date created', blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     media_files = models.ManyToManyField(MediaFile, blank=True, null=True)
     # reporter = models.ForeignKey(User, on_delete=models.PROTECT)
 
