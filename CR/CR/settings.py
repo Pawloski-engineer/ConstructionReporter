b09d27110be16755e6cc27c37ca044d9314e9172
pkbla <pkblasiak@gmail.com>
2021-05-30 23:41:38 +0200

    changes CORS settings to allowa all

--- CR/CR/settings.py
+++ CR/CR/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ConstructionReporter',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
